6298b774219da6f294840b34f5958c8f1141df7b
CJ <67108005+cjh91@users.noreply.github.com>
2021-06-16 00:07:25 +0900

    Delete 17_Interpreter directory

--- 17_Interpreter/src/sample_interpreter/Node.java
+++
@@ -1,5 +0,0 @@
-package sample_interpreter;
-
-public abstract class Node {
-    public abstract void parse(Context context) throws ParseException;
-}
--- 17_Interpreter/src/sample_interpreter/ParseException.java
+++
@@ -1,7 +0,0 @@
-package sample_interpreter;
-
-public class ParseException extends Exception {
-	public ParseException(String msg) {		// Constructor ParseException
-		super(msg); 	
-	}
-}
